bcd70e0a1944fe1254e4ba93311e083c021dc3bd
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2025-03-31 11:15:35 +1000

    Update testharness.py

--- .devcontainer/testing/testharness.py
+++ .devcontainer/testing/testharness.py
@@ -64,7 +64,7 @@ for step in steps:
 
         if output.returncode != 0 and DEV_MODE == "FALSE":
             logger.error(f"Must create an issue: {step} {output}")
-            create_github_issue(output, step_name=step)
+            #create_github_issue(output, step_name=step)
         else:
             print(output)
     else:
@@ -72,6 +72,6 @@ for step in steps:
         print(f"[{step}] | {output.returncode} | {output.stdout}")
         if output.returncode != 0 and DEV_MODE == "FALSE":
             logger.error(f"Must create an issue: {step} {output}")
-            create_github_issue(output, step_name=step)
+            #create_github_issue(output, step_name=step)
         else:
             print(output)
